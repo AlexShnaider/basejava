c35d23b7e4bd109c659419066b4c499dc303cc59
Alexander Shnaider <shnaider.sasha@gmail.com>
2017-12-13 02:57:16 +0300

    home work1, first attempt

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,45 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        storage[getAll().length] = r;
     }
 
     Resume get(String uuid) {
+        for (Resume resume : getAll()) {
+            if (resume.toString().equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < getAll().length; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, storage.length - i - 1);
+                storage[storage.length - 1] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int amountOfResumes = 0;
+        while (storage[amountOfResumes] != null) {
+            amountOfResumes++;
+        }
+        Resume[] notNullResumes = new Resume[amountOfResumes];
+        System.arraycopy(storage, 0, notNullResumes, 0, amountOfResumes);
+        return notNullResumes;
     }
 
     int size() {
-        return 0;
+        return getAll().length;
     }
 }
