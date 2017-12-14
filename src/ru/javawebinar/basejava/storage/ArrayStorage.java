cc5d5545788ef88d385a2ff1d535926b4110817e
Alexander Shnaider <shnaider.sasha@gmail.com>
2017-12-15 00:20:55 +0300

    home work2, adding package, getters, setters, equals and hashCode for Resume

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -1,39 +1,46 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
 import java.util.Arrays;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+
+    private static final int MAX_VALUE = 10000;
+
+    private final Resume[] storage = new Resume[MAX_VALUE];
     private int size;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, null);
         size = 0;
     }
 
-    void save(Resume r) {
-        if (getIndexOfResume(r.uuid) >= 0) {
+    public void save(Resume r) {
+        if (getIndexOfResume(r.getUuid()) != -1) {
             System.out.println("The resume is already exist in the ArrayStorage");
-        } else if (size < storage.length) {
-            storage[size++] = r;
-        } else {
+        } else if (size == MAX_VALUE) {
             System.out.println("ArrayStorage overflow");
+        } else {
+            storage[size++] = r;
         }
     }
 
-    void update(Resume r) {
-        int index = getIndexOfResume(r.uuid);
-        if (index < 0) {
+    public void update(Resume r) {
+        int index = getIndexOfResume(r.getUuid());
+        if (index == -1) {
             noResumeMessage();
         } else {
             storage[index] = r;
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         int index = getIndexOfResume(uuid);
-        if (index < 0) {
+        if (index == -1) {
             noResumeMessage();
             return null;
         } else {
@@ -41,12 +48,10 @@ public class ArrayStorage {
         }
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = getIndexOfResume(uuid);
-        if (index < 0) {
+        if (index == -1) {
             noResumeMessage();
-        } else if (size == 1) {
-            storage[--size] = null;
         } else {
             storage[index] = storage[--size];
             storage[size] = null;
@@ -57,17 +62,17 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
     private int getIndexOfResume(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
+            if (storage[i].getUuid().equals(uuid)) {
                 return i;
             }
         }
