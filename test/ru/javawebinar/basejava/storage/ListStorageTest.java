5ec9634ac2a95b2db89d92fb9bfd745a81c2c77c
Alexander Shnaider <shnaider.sasha@gmail.com>
2017-12-19 01:57:32 +0300

    tests modifications

---
+++ test/ru/javawebinar/basejava/storage/ListStorageTest.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.Arrays;
+
+public class ListStorageTest extends AbstractArrayStorageTest {
+
+    public ListStorageTest() {
+        super(new ListStorage());
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        Resume[] answer = storage.getAll();
+        Arrays.sort(answer);
+        Assert.assertTrue(Arrays.binarySearch(answer, RESUME1) >= 0);
+        Assert.assertTrue(Arrays.binarySearch(answer, RESUME2) >= 0);
+        Assert.assertTrue(Arrays.binarySearch(answer, RESUME3) >= 0);
+    }
+}
